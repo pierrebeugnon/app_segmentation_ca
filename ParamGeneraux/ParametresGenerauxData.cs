4d5eaa3e00e6aa6b2a51b0fa7b0324d91440d8c7
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 17:30:49 +0200

    Create ParametresGenerauxData.cs

---
+++ ParamGeneraux/ParametresGenerauxData.cs
@@ -0,0 +1,10 @@
+namespace Segmentation.Shared.Models
+{
+    public class ParametresGenerauxData
+    {
+        public int ParametresGenerauxID { get; set; }
+
+        public double HeuresParSemaine { get; set; }
+        public double NbSemainesParAn { get; set; }
+    }
+}
