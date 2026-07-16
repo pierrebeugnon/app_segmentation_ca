ae7b72f855105acd1d8587ac29ccd8433300c6d8
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 15:55:02 +0200

    Create GetAllConseillerProfilQuery.cs

---
+++ bdd/SegmentsIntensite/GetAllConseillerProfilQuery.cs
@@ -0,0 +1,10 @@
+using Segmentation.Application.Contracts;
+using Segmentation.Application.Responses;
+
+namespace Segmentation.Application.Queries.ConseillerProfil
+{
+    public class GetAllConseillerProfilQuery
+        : IQuery<List<ConseillerProfilResponse>>
+    {
+    }
+}
