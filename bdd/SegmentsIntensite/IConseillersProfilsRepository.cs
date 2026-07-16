6f5f52704d7e0699c505ef8910899b59bfd877e4
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 15:38:51 +0200

    Create IConseillersProfilsRepository.cs

---
+++ bdd/SegmentsIntensite/IConseillersProfilsRepository.cs
@@ -0,0 +1,9 @@
+using Segmentation.Core.Entities;
+
+namespace Segmentation.Core.Repositories
+{
+    public interface IConseillersProfilsRepository
+        : IAsyncRepository<ConseillersProfils>
+    {
+    }
+}
