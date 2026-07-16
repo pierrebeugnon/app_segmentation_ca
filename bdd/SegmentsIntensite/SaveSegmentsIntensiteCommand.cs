977d8702350a6051d5ebc29b91b222cb1850c883
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 14:50:46 +0200

    Update SaveSegmentsIntensiteCommand.cs

--- bdd/SegmentsIntensite/SaveSegmentsIntensiteCommand.cs
+++ bdd/SegmentsIntensite/SaveSegmentsIntensiteCommand.cs
@@ -1,10 +1,17 @@
-using Segmentation.Application.Contracts;
-using Segmentation.Shared.Models;
+using MediatR;
 
 namespace Segmentation.Application.Commands.SegmentIntensite
 {
-    public class SaveSegmentsIntensiteCommand : ICommand<int>
+    public class SaveSegmentsIntensiteCommand : IRequest<int>
     {
-        public required List<SegmentIntensiteData> Items { get; set; }
+        public List<SaveSegmentIntensiteItem> Items { get; set; } = new();
+    }
+
+    public class SaveSegmentIntensiteItem
+    {
+        public string LigneMetier { get; set; } = "";
+        public string Segment { get; set; } = "";
+        public double NombreRdvParAn { get; set; }
+        public double DureeRdvHeures { get; set; }
     }
 }
