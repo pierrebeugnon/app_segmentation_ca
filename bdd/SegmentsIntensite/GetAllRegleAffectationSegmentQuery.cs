4e81e3bd53830f4e211f1b160e39180f193b553d
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 16:25:00 +0200

    Create GetAllRegleAffectationSegmentQuery.cs

---
+++ bdd/SegmentsIntensite/GetAllRegleAffectationSegmentQuery.cs
@@ -0,0 +1,10 @@
+using Segmentation.Application.Contracts;
+using Segmentation.Application.Responses;
+
+namespace Segmentation.Application.Queries.RegleAffectationSegment
+{
+    public class GetAllRegleAffectationSegmentQuery
+        : IQuery<List<RegleAffectationSegmentResponse>>
+    {
+    }
+}
