b9c62f84789814df27f8f5f91641c957a6e427ec
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 17:29:57 +0200

    Create GetParametresGenerauxQuery.cs

---
+++ ParamGeneraux/GetParametresGenerauxQuery.cs
@@ -0,0 +1,10 @@
+using Segmentation.Application.Contracts;
+using Segmentation.Application.Responses;
+
+namespace Segmentation.Application.Queries.ParametresGeneraux
+{
+    public class GetParametresGenerauxQuery
+        : IQuery<ParametresGenerauxResponse?>
+    {
+    }
+}
