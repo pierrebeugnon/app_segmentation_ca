1c47404dd25637fd89a126b42e0fea4035645205
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 17:29:41 +0200

    Create ParametresGenerauxResponse.cs

---
+++ ParamGeneraux/ParametresGenerauxResponse.cs
@@ -0,0 +1,10 @@
+namespace Segmentation.Application.Responses
+{
+    public class ParametresGenerauxResponse
+    {
+        public int ParametresGenerauxID { get; set; }
+
+        public double HeuresParSemaine { get; set; }
+        public double NbSemainesParAn { get; set; }
+    }
+}
