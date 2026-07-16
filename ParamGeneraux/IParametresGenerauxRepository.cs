b0f98e7e6bc9d39e0144f868dce5604d9a0f7627
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-16 17:38:23 +0200

    Create IParametresGenerauxRepository.cs

---
+++ ParamGeneraux/IParametresGenerauxRepository.cs
@@ -0,0 +1,10 @@
+using Segmentation.Core.Entities;
+
+namespace Segmentation.Core.Repositories
+{
+    public interface IParametresGenerauxRepository
+        : IAsyncRepository<ParametresGeneraux>
+    {
+    }
+}
+`
