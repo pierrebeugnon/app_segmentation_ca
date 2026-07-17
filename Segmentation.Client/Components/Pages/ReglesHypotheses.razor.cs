0ec17f3c361cbca213831d676581b3ac7aaf25c5
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-17 15:12:29 +0200

    Create ReglesHypotheses.razor.cs

---
+++ Segmentation.Client/Components/Pages/ReglesHypotheses.razor.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+using Segmentation.Client.Models;
+using Segmentation.Client.Services;
+using Segmentation.Shared.Models;
+using System.Globalization;
+using System.Text;
+
+namespace Segmentation.Client.Pages;
+
+public partial class ReglesHypotheses
+{
+    // Ici tu colles tout le contenu actuel du @code,
+    // mais SANS écrire @code { ... }
+}
