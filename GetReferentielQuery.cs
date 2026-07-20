5e534f732f1334f0d2934dba4416936cd59f4296
pierrebeugnon <35962071+pierrebeugnon@users.noreply.github.com>
2026-07-20 11:54:16 +0200

    Create GetReferentielQuery.cs

---
+++ GetReferentielQuery.cs
@@ -0,0 +1,10 @@
+using Segmentation.Application.Contracts;
+using Segmentation.Application.Responses;
+
+namespace Segmentation.Application.Queries.SegmentationDistributive
+{
+    public class GetReferentielQuery
+        : IQuery<ReferentielResponse>
+    {
+    }
+}
